f73604af2c1416e986bf3c750a2b0a3e6fb35666
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-02-08 23:51:38 +0000

    Create create page

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const homeController = require('../controllers/homeController');
 const detailsController = require('../controllers/detailsController');
+const createController = require('../controllers/createController');
 const defaultController = require('../controllers/defaultController');
 
 router.use(homeController);
 router.use(detailsController);
+router.use(createController);
 router.use(defaultController);
 
 
